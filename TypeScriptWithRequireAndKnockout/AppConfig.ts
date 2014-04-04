701403c5674e207ff67a765e8ae37581e64c54e6
iduncans <ian.duncanson@honeywell.com>
2014-04-04 14:18:20 +1100

    Initial commit

---
+++ TypeScriptWithRequireAndKnockout/AppConfig.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="./typings/require.d.ts" />
+/// <reference path="./typings/knockout.d.ts" />
+
+require.config({
+    paths: {
+    },
+    shim: {
+    }
+
+});
+
+require(["App"], (App) => {
+    window.addEventListener('load', () => {
+        var app = new App.App();
+        app.run();
+    });
+});
---
+++ TypeScriptWithRequireAndKnockout/ViewModels/AppViewModel.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="../typings/require.d.ts" />
+/// <reference path="../typings/knockout.d.ts" />
+/// <reference path="../typings/knockout.es5.d.ts" />
+
+export class AppViewModel {
+
+    title : string;
+
+    constructor() {
+        ko.track(this);
+        this.title = 'My Sample';
+    }
+}
